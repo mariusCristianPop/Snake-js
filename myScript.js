c47b1b20ea66d310267984289ebdc7f6134f60f6
sicabboy1 <sicabboy@gmail.com>
2022-03-23 21:14:30 +0100

    Made the snake move in one direction only
    Made the snake move towards right and stop at the width of the canvas

--- myScript.js
+++ myScript.js
@@ -4,20 +4,34 @@ var snake = {
     y: 100,
     tail: [],
     growTail: function(x, y) { // method show
-        this.tail.push(x, y)
+        this.tail.push({x:x, y:y})
         console.log(`Added to tail: ${x}, ${y}`);
-    },
-    shrinkTail: function() {
-        console.log(`Removed from tail ${this.tail.pop()}`);
-        this.tail.pop()
     }
-  };
+};
+
 
   // Defining what appears on the canvas when the page loads
   function setup() {
     createCanvas(800, 600);
     background(64);
     for (let i = 0; i < 5; ++i) {
-        ellipse(snake.x += 10, snake.y, 20, 20);
+        snake.growTail(snake.x += 10, snake.y)
+    }
+}
+
+function draw() {
+    background(64);
+    stroke(50)
+    fill(255)
+    for (let i = 0; i < snake.tail.length; ++i) { // draw the snake's tail
+        console.log("drawing")
+        ellipse(snake.tail[i].x += 1, snake.tail[i].y, 24, 24);
+        
+    }
+    snake.tail.pop()
+    snake.growTail(snake.x += 1, snake.y)
+    if (snake.x > width || snake.x < 0 || snake.y < 0 || snake.y > height) {
+        console.log("Game over")
+        noLoop()
     }
 }
