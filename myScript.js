3ba7b83c5ad71f731bbe58bf32c9bb140f6b03af
Marius Pop <30757467+mariusCristianPop@users.noreply.github.com>
2022-03-25 19:57:52 +0100

    Game over on tail touch

--- myScript.js
+++ myScript.js
@@ -1,4 +1,4 @@
-
+var bodySquareSize = 24
 // created the snake object with it's proprieties and methods
 var snake = {
     x: 100,
@@ -6,22 +6,22 @@ var snake = {
     speed: 25,
     tail: [],
     direction: "",
-    tailSize: 4,
+    tailSize: 5,
     growTail: function(x, y) { // method show
-        this.tail.push({x:x, y:y})
-        console.log(`Added to tail: ${x}, ${y}`);
+        this.tail.push({x:Math.floor(x), y:Math.floor(y)})
+        //console.log(`Added to tail: ${x}, ${y}`);
     }
 };
 
   // p5.js setup function
   function setup() {
     createCanvas(800, 600);
-    frameRate(5)
     background(64);
     for (let i = 0; i < 5; ++i) {
-        snake.growTail(snake.x += i, snake.y)
+        snake.growTail(snake.x += bodySquareSize, snake.y)
     }
     snake.direction = ""
+    frameRate(5)
 }
 // p5.js draw function
 function draw() {
@@ -36,8 +36,8 @@ function draw() {
 // Draw the snake's tail
 function snakeMoves() {
     for (let i = 0; i < snake.tail.length; ++i) {
-        console.log("drawing")
-        square(snake.tail[i].x, snake.tail[i].y, 24);
+        //console.log("drawing")
+        rect(snake.tail[i].x, snake.tail[i].y, bodySquareSize, bodySquareSize);
     }
     if (snake.tail.length > snake.tailSize) {
         snake.tail.shift()
@@ -62,15 +62,28 @@ function snakesDirection() {
 
 // Check if we have not touched the canvas borders
 function lifeCheck() {
-    if (snake.x > width || snake.x < 0 || snake.y < 0 || snake.y > height) {
+    if (snake.x > width || snake.x < 0 || snake.y < 0 || snake.y > height) { //
         console.log("Game over")
         noLoop()
+    }
+    //console.log(`Snake x: ${snake.x}`)
+    for (let i = 0; i < snake.tail.length - 1; ++i) {
+        console.log(snake.tail[i].x + " | " + snake.tail[i].y)
+        console.log(snake.tail[snake.tail.length - 1].x + " | " + snake.tail[snake.tail.length - 1].y)
+        if (snake.tail[i].x - bodySquareSize / 2 < snake.tail[snake.tail.length - 1].x + bodySquareSize / 2 &&
+            snake.tail[i].x + bodySquareSize / 2 > snake.tail[snake.tail.length - 1].x - bodySquareSize / 2 &&
+            snake.tail[i].y - bodySquareSize / 2 < snake.tail[snake.tail.length - 1].y + bodySquareSize / 2 &&
+            snake.tail[i].y + bodySquareSize / 2 > snake.tail[snake.tail.length - 1].y - bodySquareSize / 2) 
+            {
+                console.log("Bingo")
+                noLoop()
+            }
     }
 }
 
 // Detect what keyboard arrow is pressed
 function keyPressed() {
-    console.log("key pressed?")
+    //console.log("key pressed?")
     if (keyCode === UP_ARROW) {
         if (snake.direction == "left" || snake.direction == "right") {
             snake.direction = "up"
